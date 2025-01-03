013e12dc3c53d4cfb315f03b38315324569d615e
shosangit <1575544985@qq.com>
2025-01-03 23:56:40 +0800

    First commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/***********************************************************************
+* Copyright (C) 2010-2011, XXX Co. Ltd.
+* All rights reserved.
+* 
+* FileName: main.c
+* Description: EMS Software Start
+* Author: Isaac Ma
+* Date: 2025-01-03
+* Others: 
+***********************************************************************/
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    
+    return 0;
+}
